202539447c52d7064dc81b2d27f5955e63cb0fe0
minhmen93 <119550691+minhmen93@users.noreply.github.com>
2023-03-15 11:00:13 +0700

    Add files via upload

---
+++ MD3_Bai6_Ke_Thua_Thuc_Hanh/src/bai6_Ke_Thua/Shape.java
@@ -0,0 +1,33 @@
+package bai6_Ke_Thua;
+
+public class Shape {
+    private String color = "green";
+    private boolean filled = true;
+
+    public Shape() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
